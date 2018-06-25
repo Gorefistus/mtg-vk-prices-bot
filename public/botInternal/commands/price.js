ba2baf3f34038e68c4733ed03211e248cf75dc83
gorefist <superkolyan98@yandex.ru>
2018-06-25 20:57:07 +0700

    Fixed for missing cards

--- public/botInternal/commands/price.js
+++ public/botInternal/commands/price.js
@@ -7,57 +7,53 @@ const MISC = require('../../common/misc');
 const cardCache = [];
 
 async function getCardPrices(parsedCardName, setCode) {
-    try {
-        let cardIndex = -1;
-        let priceString = '';
+    let cardIndex = -1;
+    let priceString = '';
 
-        const cardObject = await MISC.getMultiverseId(parsedCardName, setCode);
-        priceString = `${cardObject.name} [${cardObject.set_name}] prices :\n TCG Mid: ${cardObject.usd ? `$${cardObject.usd}` : STRINGS.NO_DATA}\n MTGO: ${cardObject.tix ? `${cardObject.tix} tix` : STRINGS.NO_DATA}`;
+    const cardObject = await MISC.getMultiverseId(parsedCardName, setCode);
+    priceString = `${cardObject.name} [${cardObject.set_name}] prices :\n TCG Mid: ${cardObject.usd ? `$${cardObject.usd}` : STRINGS.NO_DATA}\n MTGO: ${cardObject.tix ? `${cardObject.tix} tix` : STRINGS.NO_DATA}`;
 
-        let cardName = cardObject.name;
-        if (cardObject.card_faces) {
-            cardName = cardObject.name.split('//')[0].trim();
-        }
-
-        // SCG PRICES SCRAPING START
-        cardCache.forEach((card, index) => {
-            if (card.name === cardObject.name && card.set === cardObject.set_name) {
-                cardIndex = index;
-            }
-        });
-        if (cardIndex >= 0) {
-            priceString = `${priceString} \n SCG: ${cardCache[cardIndex].value}`;
-        } else {
-            const starCityPage = await request(`${CONSTANTS.STAR_CITY_PRICE_LINK}${encodeURIComponent(cardName)}&auto=Y`);
-            const scgPriceObject = MISC.getStarCityPrice(starCityPage, cardObject);
-            if (scgPriceObject) {
-                cardCache.push({
-                    ...scgPriceObject,
-                    name: cardObject.name,
-                });
-                priceString =
-                    `${priceString} \n SCG:${scgPriceObject.set !== cardObject.set_name ? ` [${scgPriceObject.set}] ` : ''} ${scgPriceObject.value}`;
-            } else {
-                priceString = `${priceString} \n SCG: ${CONSTANTS.STAR_CITY_PRICE_LINK}${encodeURIComponent(cardName)}&auto=Y`;
-            }
-        }
-        // TOPDECK PRICES SCRAP START
-
-        const topDeckPrices = await request({
-            method: 'GET',
-            uri: `${CONSTANTS.TOPDECK_PRICE_LINK}${encodeURIComponent(cardName)}`,
-            ecdhCurve: 'auto',
-            json: true,
-        });
-        const filterByName = topDeckPrices.filter(price => price.eng_name.toLowerCase() === cardName.toLowerCase());
-        if (filterByName.length > 0) {
-            priceString = `${priceString} \n TopDeck(unknown edition): ${filterByName[0].cost} RUB`;
-        }
-        return priceString;
-
-    } catch (e) {
-        console.log(e);
+    let cardName = cardObject.name;
+    if (cardObject.card_faces) {
+        cardName = cardObject.name.split('//')[0].trim();
     }
+
+    // SCG PRICES SCRAPING START
+    cardCache.forEach((card, index) => {
+        if (card.name === cardObject.name && card.set === cardObject.set_name) {
+            cardIndex = index;
+        }
+    });
+    if (cardIndex >= 0) {
+        priceString = `${priceString} \n SCG: ${cardCache[cardIndex].value}`;
+    } else {
+        const starCityPage = await request(`${CONSTANTS.STAR_CITY_PRICE_LINK}${encodeURIComponent(cardName)}&auto=Y`);
+        const scgPriceObject = MISC.getStarCityPrice(starCityPage, cardObject);
+        if (scgPriceObject) {
+            cardCache.push({
+                ...scgPriceObject,
+                name: cardObject.name,
+            });
+            priceString =
+                `${priceString} \n SCG:${scgPriceObject.set !== cardObject.set_name ? ` [${scgPriceObject.set}] ` : ''} ${scgPriceObject.value}`;
+        } else {
+            priceString = `${priceString} \n SCG: ${CONSTANTS.STAR_CITY_PRICE_LINK}${encodeURIComponent(cardName)}&auto=Y`;
+        }
+    }
+    // TOPDECK PRICES SCRAP START
+
+    const topDeckPrices = await request({
+        method: 'GET',
+        uri: `${CONSTANTS.TOPDECK_PRICE_LINK}${encodeURIComponent(cardName)}`,
+        ecdhCurve: 'auto',
+        json: true,
+    });
+    const filterByName = topDeckPrices.filter(price => price.eng_name.toLowerCase() === cardName.toLowerCase());
+    if (filterByName.length > 0) {
+        priceString = `${priceString} \n TopDeck(unknown edition): ${filterByName[0].cost} RUB`;
+    }
+    return priceString;
+
 }
 
 
@@ -75,11 +71,14 @@ function addPriceCommand(bot) {
                 .then((prices) => {
                     bot.send(prices, message.peer_id);
                 }, (reason) => {
-                    bot.send(STRINGS.PRICES_ERR_GENERAL, message.peer_id);
-                    console.log(reason);
+                    if (reason && reason.statusCode && reason.statusCode === 404) {
+                        const options = { forward_messages: message.id };
+                        return bot.send(STRINGS.CARD_NOT_FOUND, message.peer_id, options);
+                    } else {
+                        bot.send(STRINGS.PRICES_ERR_GENERAL, message.peer_id);
+                    }
                 })
                 .catch(reason => {
-                    console.log(reason);
                     bot.send(STRINGS.PRICES_ERR_GENERAL, message.peer_id);
                 });
         });
