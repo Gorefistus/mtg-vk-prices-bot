ac781355a214decea7e059151024d513f1b2398a
gorefist <superkolyan98@yandex.ru>
2018-07-07 12:15:23 +0700

    App is now pinging itself

--- public/index.js
+++ public/index.js
@@ -1,14 +1,20 @@
 const { Bot } = require('node-vk-bot');
 const path = require('path');
 const express = require('express');
+const http = require("http");
 
 const botStarter = require('./botInternal/bot-config');
 // THIS IS JUST NEEDED SO HEROKU WON"T STOPP OUR APPLICATION
 const app = express();
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.listen(process.env.PORT || 5000);
-// __________________________________________________________
 
+setInterval(() => {
+    console.log('PINGED YOURSELF');
+    http.get('http://mtgvkbotprices.herokuapp.com/');
+}, 300000);
+
+// __________________________________________________________
 const bot = new Bot({
     token: process.env.VK_TOKEN || 'place your token here',
     prefix: /^!mth[\s,]|^!m[\s]/i,
