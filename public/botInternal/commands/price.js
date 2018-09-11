4e697e3633ff7810c2770f213d0f2e08f50e62c1
gorefist <superkolyan98@yandex.ru>
2018-09-12 00:30:56 +0300

    Log level fix

--- public/botInternal/commands/price.js
+++ public/botInternal/commands/price.js
@@ -131,7 +131,7 @@ function addPriceCommand(bot, stats) {
             testOnBorrow: true, // default
             // For all opts, see opts at https://github.com/coopernurse/node-pool#createpool
             phantomArgs: [['--ignore-ssl-errors=true', '--disk-cache=true'], {
-                logLevel: 'debug',
+                logLevel: 'error',
             }], // arguments passed to phantomjs-node directly, default is `[]`. For all opts, see https://github.com/amir20/phantomjs-node#phantom-object-api
         });
 
