02ab19130c8630865353102bd2d803ffae379cdb
Borisov Dmitry <dm.boriosv@bssys.com>
2019-04-08 17:43:55 +0300

    card command new upload method

--- src/bot-commands/card.ts
+++ src/bot-commands/card.ts
@@ -1,9 +1,10 @@
-import VK, { Keyboard, MessageContext } from 'vk-io';
+import VK, { MessageContext } from 'vk-io';
 
 import { REGEX_CONSTANTS } from '../utils/constants';
 import { ERRORS } from '../utils/strings';
 import { CommandInterface } from '../types/command';
 import { getCardByName } from "../utils/scryfall-utils";
+import { Card } from 'scryfall-sdk';
 
 
 export default class CardCommand implements CommandInterface {
@@ -49,12 +50,40 @@ export default class CardCommand implements CommandInterface {
             return this.processError(ERRORS.CARD_NO_CARD, msg);
         }
         const splittedCardNames = cardNames.split(';');
+        if (splittedCardNames.length > 0) {
+            // console.log(splittedCardNames[0]);
+        }
 
-        if (cardNames.length > 0) {
+        if (splittedCardNames.length > 0) {
             try {
-                const foundCard = await getCardByName(cardNames);
-                console.log(msg);
-                await msg.sendPhoto(foundCard.image_uris.normal);
+                const foundCardArray: Array<Card> = [];
+                for (const cardName of splittedCardNames) {
+                    if (cardName.trim().length > 0) {
+                        const cardSetSplit = cardName.match(/(.*)\[(.{3,4})\]/i);
+                        let foundCard: Card;
+                        if (cardSetSplit !== null) {
+                            foundCard = await getCardByName(cardSetSplit[1], cardSetSplit[2]);
+                        } else {
+                            foundCard = await getCardByName(cardName);
+                        }
+                        foundCardArray.push(foundCard);
+                        // this.vkBotApi.upload.messagePhoto();
+                    }
+                }
+                // console.log(foundCardArray);
+                const uploadRequestArray  = [];
+                foundCardArray.forEach(card => {
+
+                });
+                const test2 = await Promise.all(foundCardArray.map(value => {
+                    return this.vkBotApi.upload.messagePhoto({source: {value: value.image_uris.normal}});
+                }));
+                console.log(test2);
+                // const test1 = await this.vkBotApi.upload.messagePhoto({source: {values: [foundCardArray[0].image_uris.normal, foundCardArray[1].image_uris.normal]}});
+                // console.log(test1);
+                // const foundCard = await getCardByName(splittedCardNames[0]);
+                // // console.log(foundCard);
+                // const id =  await msg.sendPhoto(foundCard.image_uris.normal);
                 // msg.send({
                 //     keyboard: Keyboard.keyboard([Keyboard.textButton({
                 //         label: 'Узнать цену',
--- src/utils/scryfall-utils.ts
+++ src/utils/scryfall-utils.ts
@@ -1,14 +1,8 @@
-import {Card} from "scryfall-sdk";
+import { Card } from "scryfall-sdk";
 
 const Scry = require('scryfall-sdk');
 
 
 export function getCardByName(cardName: string, setCode?: string): Promise<Card> {
-    return new Promise<Card>((resolve, reject) => {
-        Scry.Cards.search(`${cardName} lang:any`).on('data', (card: Card) => {
-            resolve(card);
-        }).on("error", (err: Error) => {
-            reject(err);
-        });
-    });
+    return Scry.Cards.byName(cardName, setCode, true);
 }
